58671822020c2da20412f083d41a2a2fb4cb8294
Thanh Hoang-Minh <hmthanhgm@gmail.com>
2025-01-13 20:44:18 +0700

    add posts

---
+++ content/es/_meta.js
@@ -0,0 +1,14 @@
+export default {
+  index: {
+    type: 'page',
+    display: 'hidden',
+    theme: {
+      timestamp: false,
+      typesetting: 'article'
+    }
+  },
+  sample: {
+    type: 'page',
+    title: 'Sample'
+  },
+}
